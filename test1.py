31054396f07f2b055f2928f02f6ee1352f28d768
Archemalit <87569578+Archemalit@users.noreply.github.com>
2022-02-08 19:18:50 +0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QComboBox
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QLineEdit, QPushButton, QComboBox
 from PyQt5.QtCore import Qt
 
 
@@ -24,15 +24,40 @@ class MainWindow(QMainWindow):
         self.typeMap = 'map'
 
         self.image = QLabel(self)
+        self.image.resize(650, 450)
+        self.image.move(75, 50)
+
         self.combobox = QComboBox(self)
         self.combobox.move(625, 510)
         self.combobox.addItems(['map', 'sat', 'skl'])
         self.combobox.setFocusPolicy(Qt.NoFocus)
         self.combobox.activated[str].connect(self.changeMapType)
-        self.image.resize(650, 450)
-        self.image.move(75, 50)
+
+        self.line = QLineEdit(self)
+        self.line.resize(200, 40)
+        self.line.move(20, 580)
+        self.line.setFocusPolicy(Qt.ClickFocus)
+
+        self.button = QPushButton("Искать", self)
+        self.button.resize(80, 40)
+        self.button.move(280, 580)
+        self.button.clicked.connect(self.update)
+        self.button.setFocusPolicy(Qt.NoFocus)
+
 
     def update(self):
+        self.line.clearFocus()
+        try:
+            if str(self.line.text()) != "":
+                coords = requests.get(
+                    f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={str(self.line.text())}&format=json").json()[
+                    "response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split()
+                self.lon = coords[0]
+                self.lat = coords[1]
+                self.line.setText("")
+        except:
+            pass
+
         lon = str(self.lon)
         lat = str(self.lat)
         params = {
@@ -41,6 +66,8 @@ class MainWindow(QMainWindow):
             "z": self.z,
             "size": ",".join(["650", "450"])
         }
+        self.lon = float(self.lon)
+        self.lat = float(self.lat)
         response = requests.get("http://static-maps.yandex.ru/1.x/", params=params)
         self.qp.loadFromData(response.content)
         self.image.setPixmap(self.qp)
@@ -55,7 +82,6 @@ class MainWindow(QMainWindow):
                 self.z -= 1
 
         value = (2 ** (17 - int(self.z)))
-
         if event.key() == Qt.Key_Up:
             if self.lat + 0.001 * value <= 180:
                 self.lat += 0.001 * value
@@ -77,8 +103,12 @@ class MainWindow(QMainWindow):
         self.typeMap = type
         self.update()
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
